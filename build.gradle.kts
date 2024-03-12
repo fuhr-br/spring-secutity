7db1a1d2dbed2cd2fcdf0b76182f1734ceb9196c
anderson_fuhr <anderson_fuhr@sicredi.com.br>
2024-03-12 20:36:57 -0300

    refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "larcerto"
+group = "api"
 version = "0.0.1-SNAPSHOT"
 
 java {
--- src/test/java/api/ApiApplicationTests.java
+++ src/test/java/api/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package larcerto.api;
+package api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
