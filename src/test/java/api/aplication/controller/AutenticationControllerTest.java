1ee85d46af948056c5c526a922faae940c15dde8
anderson_fuhr <anderson_fuhr@sicredi.com.br>
2024-03-27 18:46:55 -0300

    add testes

--- src/test/java/api/aplication/controller/AutenticationControllerTest.java
+++ src/test/java/api/aplication/controller/AutenticationControllerTest.java
@@ -9,45 +9,72 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.springframework.security.test.context.support.WithMockUser;
-
 import static api.aplication.helper.SqlProvider.INSERT_USUARIO;
 import static api.aplication.helper.SqlProvider.RESETAR_DB;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class AutenticationControllerTest {
 
-    @Autowired
-    private MockMvc restClient;
+  @Autowired
+  private MockMvc restClient;
 
-    @Autowired
-    private final ObjectMapper mapper = new ObjectMapper();
+  @Autowired
+  private final ObjectMapper mapper = new ObjectMapper();
 
-    AutenticacaoDTO autenticacaoDTO = AutenticacaoDTOFixture.builder();
+  AutenticacaoDTO autenticacaoDTO = AutenticacaoDTOFixture.builder();
+
+  @Test
+  @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = RESETAR_DB)
+  @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = INSERT_USUARIO)
+  @DisplayName("Deve logar um usuario corretamente")
+  void deveGerarUmTokenDeUsuario() throws Exception {
+
+    String requisicao = mapper.writeValueAsString(autenticacaoDTO);
+
+    this.restClient.perform(post("/v1/api/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requisicao))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.token").isString())
+        .andExpect(jsonPath("$.token").isNotEmpty());
+
+  }
+
+  @Test
+  @WithMockUser(username = "username", roles = {"USUARIO"})
+  @DisplayName("Deve buscar um recurso")
+  void deveBuscarUmRecurso() throws Exception {
+
+    String requisicao = mapper.writeValueAsString(autenticacaoDTO);
+
+    this.restClient.perform(get("/v1/api/auth")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(requisicao))
+        .andExpect(status().isOk())
+        .andExpect(content().string("Busca com sucesso"));
+
+  }
 
     @Test
-    @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = RESETAR_DB)
-    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = INSERT_USUARIO)
-    @DisplayName("Deve logar um usuario corretamente")
-    void deveGerarUmTokenDeUsuario() throws Exception {
+    @WithMockUser(username = "username", roles = {"ADMINISTRADOR"})
+    @DisplayName("Deve retornar código http  403 ao buscar um recurso com role invalida")
+    void deveRecusarABuscaDeUmRecurso() throws Exception {
 
         String requisicao = mapper.writeValueAsString(autenticacaoDTO);
 
-        this.restClient.perform(post("/v1/api/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requisicao))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").isString())
-                .andExpect(jsonPath("$.token").isNotEmpty());
+        this.restClient.perform(get("/v1/api/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requisicao))
+            .andExpect(status().isForbidden());
 
     }
-
 
 }
